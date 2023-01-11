65497642e87a8146799acca13a75626862958e1d
Amit T. Dorle <Amitdorle111@gmail.com>
2023-01-11 12:36:30 +0530

    Signup page Changed

--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,99 +1,142 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import swal from 'sweetalert';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import swal from "sweetalert";
 
-import {currentUser} from './../../util/currentUser'
-import "./Signup.css"
+import SignUp from './signup.png'
+import { currentUser } from "./../../util/currentUser";
+import "./Signup.css";
 
 function Signup() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const [password, setPassword] = useState('')
-  const [role, setRole] = useState('user')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("user");
 
   useEffect(() => {
-    if(currentUser){
-     window.location.href="/"
+    if (currentUser) {
+      window.location.href = "/";
     }
-  }, [])
+  }, []);
 
   async function signupUser() {
-   const response = await axios.post('/signup', {
+    const response = await axios.post("/signup", {
       name: name,
       email: email,
       phone: phone,
       password: password,
-      role: role
-   })
-   console.log(response.data)
-   if(response.data.success){
-     await swal({
-      title: "Success",
-      text: response.data.message,
-      icon: "success",
-      button: "Aww yiss!",
+      role: role,
     });
-     window.location.href = '/login'
-   }
-    else{
+    console.log(response.data);
+    if (response.data.success) {
+      await swal({
+        title: "Success",
+        text: response.data.message,
+        icon: "success",
+        button: "Aww yiss!",
+      });
+      window.location.href = "/login";
+    } else {
       swal({
         title: "Error",
         text: response.data.message,
         icon: "error",
         button: "Try Again!",
       });
-      setName('')
-      setEmail('')
-      setPhone('')
-      setPassword('')
+      setName("");
+      setEmail("");
+      setPhone("");
+      setPassword("");
     }
   }
 
   return (
     <div>
-      <h1 className='text-center'>Signup</h1>
-      <div className='row'>
-       <div className='col-md-6'>
+      <h1 className="text-center">Signup</h1>
+      <div className="row">
+        <div className="col-md-6"></div>
 
-       </div>
+        <div className="col-md-6">
+          <div className="Main-signup-container">
+            <div className="signup-container">
+            <div className="text-center">
+                    <img src={SignUp} height="90px"/>
+                </div>
+                <div className="m-3">
+                    <h2 className="Signup-head">
+                        Sign
+                        Up and Run For Food</h2>
+                </div>
+                <div className="m-1">
+                    <p className="text-center">Let's signed up and go straight to the food.</p>
+                </div>
+              <form>
+                <div className="form-containerr">
+                <div>
+                  <h6>Full Name: </h6>
+                  <input
+                    type="text"
+                    id="name"
+                    placeholder="Enter Name"
+                    className="user-input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
 
-       <div className='col-md-6'>
-        <div className='form-container'>
-          <form>
-           <div>
-              <label htmlFor='name'>Full Name: </label>
-              <input type='text' id='name' placeholder='Enter Name' className='user-input'
-              value={name} onChange={(e) => setName(e.target.value)}/>
-           </div>
+                <div>
+                  <h6>Email: </h6>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Enter Email"
+                    className="user-input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
 
-           <div>
-              <label htmlFor='email'>Email: </label>
-              <input type='email' id='email' placeholder='Enter Email' className='user-input'
-              value={email} onChange={(e) => setEmail(e.target.value)}/>
-           </div>
+                <div>
+                  <h6>Phone: </h6>
+                  <input
+                    type="text"
+                    id="phone"
+                    placeholder="Enter Phone"
+                    className="user-input"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </div>
 
-           <div>
-              <label htmlFor='phone'>Phone: </label>
-              <input type='text' id='phone' placeholder='Enter Phone' className='user-input'
-              value={phone} onChange={(e) => setPhone(e.target.value)}/>
-           </div>
+                <div>
+                  <h6>Password: </h6>
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder="Enter Password"
+                    className="user-input"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                </div>
 
-           <div>
-              <label htmlFor='password'>Password: </label>
-              <input type='password' id='password' placeholder='Enter Password' className='user-input'
-              value={password} onChange={(e) => setPassword(e.target.value)}/>
-           </div>
-
-            <div>
-              <button type='button' className='signup-button' onClick={signupUser}>Signup</button>
+                <div className="sign-up-button-container">
+                  <button
+                    type="button"
+                    className="sign-up-button"
+                    onClick={signupUser}
+                  >
+                    <b>Signup</b>
+                  </button>
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
         </div>
       </div>
-      </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
