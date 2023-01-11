787d2380d21cc24539f0142eaf3e080af0341141
Amit T. Dorle <Amitdorle111@gmail.com>
2023-01-11 13:12:22 +0530

    Css improve

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,6 +1,7 @@
 import React, {useState,useEffect} from 'react'
 import axios from 'axios'
 import swal from 'sweetalert';
+import { Link } from 'react-router-dom';
 
 import {currentUser} from './../../util/currentUser'
 import "./Login.css"
@@ -61,17 +62,16 @@ function Login() {
                 <div>
                     <p className="welcome-text">WELCOME BACK</p>
                 </div>
-                <div className="text-center">
+                <div className="text-center emoji">
                     😃
                 </div>
-                <div className="m-3">
-                    <h2 className="Signup-head">
-                        Sign
-                        In to
+                <div className="m-4">
+                    <h2 className="Signin-head">
+                        Sign In to
                         Your Account</h2>
                 </div>
-                <div className="m-1">
-                    <p className="text-center">Let's get you signed in and straight to the jobs.</p>
+                <div>
+                    <p className="text-center">Let's get you signed in and straight to the Food.</p>
                 </div>
                 <div className="form-container m-3">
                     <form>
@@ -102,14 +102,14 @@ function Login() {
                     <div> <a href="/pages/forget-password.html">Forgot your password?</a></div>
                 </div>
                 <div className="sign-in-button-container">
-                    <button type="button" class="sign-in-button" onClick={loginUser}><b>Sign In</b></button>
+                    <button type="button" class="sign-in-button" onClick={loginUser}><b>Login</b></button>
                 </div>
             </div>
             <div className="sign-up-link-section">
                 <div>
                     <p>Don;t have an account?</p>
                 </div>
-                <div><a href="/pages/Sign-Up.html">Sign up and get started!</a></div>
+                <div><Link to="/Signup">Sign up and get started!</Link></div>
             </div>
         </div>
     </div>
